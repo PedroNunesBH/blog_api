fb959bac47058c9207b0ceb5122bac6b3c68672d
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-18 18:08:56 -0300

    Adicionado modelo de usuarios do blog (BlogUser)

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import BlogUser
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in BlogUser._meta.fields]
+    search_fields = ('name', )
+
+
+admin.site.register(BlogUser, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+
+class BlogUser(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    user = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    register_date = models.DateTimeField(auto_now_add=True)
+    date_of_birth = models.DateField()
