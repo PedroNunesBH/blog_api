47a6738efdee8b32feee2ab1d001d18e633738a3
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-19 17:59:40 -0300

    Adicionado model para postagem dos usuarios (Post).

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class AdminPost(admin.ModelAdmin):
+    list_display = [field.name for field in Post._meta.fields]
+    search_fields = ('title', )
+
+
+admin.site.register(Post, AdminPost)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import BlogUser
 
-# Create your models here.
+
+class Post(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    author = models.ForeignKey(BlogUser,
+                               on_delete=models.PROTECT,
+                               related_name="post_author_user")  # ID do usuário autor do post
+    content = models.CharField(max_length=500)
