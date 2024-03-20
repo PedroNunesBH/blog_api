f13aab876714b4c1786b76338c63223d24691009
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-20 16:29:26 -0300

    Criacao da view para obter o acess e refresh JWT Token (get_token).

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-
+    path('get_token/', TokenObtainPairView.as_view(), name="get_token")
 ]
