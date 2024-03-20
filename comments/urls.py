a523cb81ccab3340a86c3dfb5e1ea50c0646a75c
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-20 15:16:26 -0300

    Adicionada view de criação de comentarios no blog (CreateComments).

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListComments
+from .views import ListComments, CreateComments
 
 urlpatterns = [
     path('list_comments/', ListComments.as_view(), name="list_comments"),
+    path('create_comments/', CreateComments.as_view(), name="create_comments"),
 ]
--- comments/views.py
+++ comments/views.py
@@ -7,3 +7,7 @@ from .models import Comment
 class ListComments(generics.ListAPIView):
     queryset = Comment.objects.all().filter(status="Aprovado")  # Apenas lista os comentarios aprovados
     serializer_class = CommentSerializer
+
+
+class CreateComments(generics.CreateAPIView):
+    serializer_class = CommentSerializer
