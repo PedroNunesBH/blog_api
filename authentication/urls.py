f779ed460f0eda2a6f85b69d6553e800f3d0db5c
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-20 16:33:45 -0300

    Criacao da view para verificar a validade do JWT Acess Token (verify_token).

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
-    path('get_token/', TokenObtainPairView.as_view(), name="get_token")
+    path('get_token/', TokenObtainPairView.as_view(), name="get_token"),
+    path('verify_token/', TokenVerifyView.as_view(), name="verify_token")
 ]
