b4e04fc90b4a1d6ec886dbe2523e5935eb10f891
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-20 15:05:56 -0300

    Adicionado arquivo de urls para a app comments.

--- comments/models.py
+++ comments/models.py
@@ -4,7 +4,7 @@ from posts.models import Post
 
 
 STATUS_CHOICES = (
-    ("Aguardando Aprovação", "Pendente"),
+    ("Pendente", "Pendente"),
     ("Aprovado", "Aprovado"),
     ("Rejeitado", "Rejeitado")
 )
@@ -20,7 +20,6 @@ class Comment(models.Model):
                                   related_name="comment_post")  # Post a qual o comentário foi feito
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    status = models.CharField(choices=STATUS_CHOICES, default="Aguardando Aprovação")
+    status = models.CharField(max_length=60, choices=STATUS_CHOICES, default="Aguardando Aprovação")
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
-
