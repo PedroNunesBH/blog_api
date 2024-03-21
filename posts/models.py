1598afdb62fb722ae2943b92342eee98a031d1fe
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-21 15:07:56 -0300

    Adicao do campo status no model Post para verificação/aprovação dos posts.

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from users.models import BlogUser
 
+STATUS_CHOICES = (
+    ("Pendente", "Pendente"),
+    ("Aprovado", "Aprovado"),
+    ("Rejeitado", "Rejeitado")
+)
+
 
 class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True)
@@ -10,6 +16,7 @@ class Post(models.Model):
                                on_delete=models.PROTECT,
                                related_name="post_author_user")  # ID do usuário autor do post
     content = models.CharField(max_length=500)
+    status = models.CharField(max_length=80, choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.title
