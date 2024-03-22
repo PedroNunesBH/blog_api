d52b732ba2ce5f04980fe40a3ad6b4e23d4355ad
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-03-22 16:46:51 -0300

    Alteração no campo category de Post para campo Choices.

--- posts/models.py
+++ posts/models.py
@@ -8,11 +8,17 @@ STATUS_CHOICES = (
     ("Rejeitado", "Rejeitado")
 )
 
+CATEGORIES_CHOICES = (
+    ("Psicologia", "Psicologia"),
+    ("Auto Ajuda", "Auto Ajuda"),
+    ("Romance", "Romance")
+)
+
 
 class Post(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, choices=CATEGORIES_CHOICES)
     author = models.ForeignKey(BlogUser,
                                on_delete=models.PROTECT,
                                related_name="post_author_user")  # ID do usuário autor do post
